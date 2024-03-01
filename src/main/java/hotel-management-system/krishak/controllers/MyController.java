05342f32cdc230a9b398e47454eb46a08e6b3177
Krishak Chhura <112682929+krishak2002@users.noreply.github.com>
2024-03-01 16:58:10 +0530

    Update MyController.java

--- src/main/java/hotel-management-system/krishak/controllers/MyController.java
+++ src/main/java/hotel-management-system/krishak/controllers/MyController.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package AIrbnb.pawandeep.controllers;
+package hotel-management-system.krishak.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
